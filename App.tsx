307ad5692d3c4aa5e34d9eaf069154c7a889f060
0w9 <79471472+0w9@users.noreply.github.com>
2023-02-02 08:27:37 +0100

    2 files modified

--- App.tsx
+++ App.tsx
@@ -1,40 +1,90 @@
 import * as React from 'react';
 import './style.css';
-import {useState} from "react"
+import { useState } from 'react';
 
 export default function App() {
+  const [goal, setGoal] = useState('built a SaaS app');
+  const [metric, setMetric] = useState('Hours');
+  const [totalMetric, setTotalMetric] = useState('10');
 
-  const [goal, setGoal] = useState("")
+  return (
+    <>
+      <nav class="bg-white p-4 shadow-lg">
+        <div class="container mx-auto flex items-center justify-center">
+          <div class="ml-4">
+            <h1 class="rounded-lg py-2 px-4 font-medium text-blue-500 hover:text-blue-700" href="#">Pricing</h1>
+          </div>
+        </div>
+      </nav>
 
-  return <div className="flex items-center justify-center h-screen">
-  <form className="w-1/2 p-6 bg-white rounded-lg shadow-lg">
-    <h2 className="text-lg font-medium mb-4">Enter Metric</h2>
-    <input className="bg-gray-200 p-2 rounded-lg w-full mb-4" type="text" placeholder="Metric" />
-    <select className="bg-gray-200 p-2 rounded-lg w-full mb-4">
-      <option>Time</option>
-      <option>xyz</option>
-    </select>
-    <input className="bg-gray-200 p-2 rounded-lg w-full mb-4" type="text" placeholder="Like 10 days"></input>
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg">Submit</button>
-  </form>
-</div>
+      <div className="flex items-center justify-center h-screen">
+        <form className="w-1/2 p-6 bg-white rounded-lg shadow-lg">
+          <h2 className="text-lg font-medium mb-4">Enter Metric</h2>
+          <input
+            className="bg-gray-200 p-2 rounded-lg w-full mb-4"
+            type="text"
+            placeholder="built a SaaS app"
+            onChange={(e) => {
+              setGoal(e.target.value);
+            }}
+          />
 
-<div className="container mx-auto mt-2">
-  <div className="flex flex-wrap">
-    <div className="w-1/2 p-4">
-      <div className="bg-white p-4 rounded-lg shadow-lg">
-        <h3 className="text-lg font-medium mb-2">🧵 Thread</h3>
-        <h2 className="text-lg font-medium mb-4">How I made 1k$ in 10 days.</h2>
-        <p className="text-gray-600 mb-4">I made 1k$ in 10 days using programming. This is how:</p>
+          <input
+            className="bg-gray-200 p-2 rounded-lg w-full mb-4"
+            type="text"
+            placeholder="10"
+            onChange={(e) => {
+              setTotalMetric(e.target.value);
+            }}
+          ></input>
+
+          <select
+            className="bg-gray-200 p-2 rounded-lg w-full mb-4"
+            onChange={(e) => {
+              setMetric(e.target.value);
+            }}
+          >
+            <optgroup label="Time">
+              <option>Hours</option>
+              <option>Days</option>
+              <option>Weeks</option>
+              <option>Months</option>
+              <option>Years</option>
+            </optgroup>
+          </select>
+
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg">
+            Submit
+          </button>
+        </form>
       </div>
-    </div>
-    <div className="w-1/2 p-4">
-      <div className="bg-white p-4 rounded-lg shadow-lg">
-        <h3 className="text-lg font-medium mb-2">🧵 Thread</h3>
-        <h2 className="text-lg font-medium mb-4">How I {goal} in 10 days.</h2>
-        <p className="text-gray-600 mb-4">I made 1k$ in 10 days using programming. This is how:</p>
+
+      <div className="container mx-auto mt-2">
+        <div className="flex flex-wrap">
+          <div className="w-1/2 p-4">
+            <div className="bg-white p-4 rounded-lg shadow-lg">
+              <h3 className="text-lg font-medium mb-2">🧵 Thread</h3>
+              <h2 className="text-lg font-medium mb-4">
+                How I {goal} in {totalMetric} {metric}.
+              </h2>
+              <p className="text-gray-600 mb-4">
+                I {goal} in 10 days using programming. This is how:
+              </p>
+            </div>
+          </div>
+          <div className="w-1/2 p-4">
+            <div className="bg-white p-4 rounded-lg shadow-lg">
+              <h3 className="text-lg font-medium mb-2">🧵 Thread</h3>
+              <h2 className="text-lg font-medium mb-4">
+                How I {goal} in {totalMetric} {metric}.
+              </h2>
+              <p className="text-gray-600 mb-4">
+                This is guide teaches you everything until you {goal} in {totalMetric} {metric}.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</div>
+    </>
+  );
 }
